5d5c3d8dd0234754b61aa9d37f696b243793cc3f
Hack Reactor Student <student@hackreactor.com>
2017-06-26 11:40:16 -0500

    finished stringifyJSON with all passing tests

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,41 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  //inputs: ints, objs, arrays, strings, undefined, null, functions, bool
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+
+  if (Array.isArray(obj)) {
+    var result = [];
+
+    for (var i = 0; i < obj.length; i++) {
+      result = result.concat(stringifyJSON(obj[i]));
+    }
+
+    return '[' + result.toString() + ']';
+  }
+
+  if (typeof obj === 'object') {
+    if (obj === null) {
+      return 'null'; 
+    }
+    
+    var output = [];
+    for (var key in obj) {
+      if (typeof obj[key] === 'function' || obj[key] === undefined) {
+        continue;
+      }
+
+      output = output.concat(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+    }
+    
+    return '{' + output.toString() + '}';
+  }
+  
+
+
+
+  return '' + obj;  
 };
+console.log();
