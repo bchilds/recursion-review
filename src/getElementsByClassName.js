ca350e0e0619c329bf4af4c6530839ea4d7ded60
Hack Reactor Student <student@hackreactor.com>
2017-06-26 15:29:48 -0500

    getElementsByClassName passes all tests, added true to Spec

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,24 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className, startingNode) {
+  //traverse DOM to check each node for class name
+  //start with starting node
+    startingNode = startingNode || document.body;
+    var output = [];
+    //get its class names
+      //check class name of node vs desired
+    if(startingNode.classList.contains(className)){
+      output.push(startingNode);
+    }
+    // check child nodes
+    for(var i = 0; i < startingNode.children.length; i++){
+      //check each child node recursively
+      console.log(startingNode.children[i]);
+      output = output.concat(getElementsByClassName(className, startingNode.children[i]));
+    }
+
+  //return an array of all nodes in this tree which contain the className
+    
+  return output;
 };
